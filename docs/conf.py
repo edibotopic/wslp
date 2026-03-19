8f91508921fb4751e3f8a293c670ba4ed217a31e
Shane Crowley <shanevcrowley@gmail.com>
2026-03-19 15:38:36 +0000

    fixup: remove image tag

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ html_context = {
     # Product tag image; the orange part of your logo, shown in the page header
     #
     # TODO: To add a tag image, uncomment and update as needed.
-    'product_tag': '_static/wslp-tag.png',
+    # 'product_tag': '_static/wslp-tag.png',
     # Your Discourse instance URL
     #
     # TODO: Change to your Discourse instance URL or leave empty.
